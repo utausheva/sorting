c715c31c3e9d8fc9db2c1b490063d26507d459a0
utausheva <ulyashk@list.ru>
2016-11-27 17:56:01 +0300

    Add files via upload

--- tests/ru/mail/polis/sort/valid/Tester.java
+++ tests/ru/mail/polis/sort/valid/Tester.java
@@ -1,10 +1,5 @@
 package ru.mail.polis.sort.valid;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import org.junit.Assert;
 import org.junit.Rule;
@@ -14,12 +9,18 @@ import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import ru.mail.polis.sort.*;
 
-import ru.mail.polis.sort.BubbleSort;
-import ru.mail.polis.sort.Helper;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RunWith(value = Parameterized.class)
 public class Tester {
+
+    private static final Random r = ThreadLocalRandom.current();
 
     @Rule
     public TestRule watcher = new TestWatcher() {
@@ -27,6 +28,15 @@ public class Tester {
             System.err.println("=== Running " + description.getMethodName());
         }
     };
+
+    private static int[] gen(int n) {
+        int[] a = new int[n];
+        for (int i = a.length - 1; i > 0; i--) {
+            int j = r.nextInt(i + 1);
+            Helper.swap(a, i, j);
+        }
+        return a;
+    }
 
     @Parameterized.Parameter
     public int[] array;
@@ -40,11 +50,11 @@ public class Tester {
             {0, 1, 1, 0},
             {1},
             {Integer.MAX_VALUE, 0, 0, Integer.MIN_VALUE},
-            Helper.gen(1),
-            Helper.gen(10),
-            Helper.gen(100),
-            Helper.gen(1000),
-            Helper.gen(10000),
+            gen(1),
+            gen(10),
+            gen(100),
+            gen(1000),
+            gen(10000),
         });
     }
 
@@ -59,6 +69,30 @@ public class Tester {
     @Test
     public void test01_checkBubbleSort() throws IOException {
         Assert.assertTrue(isSorted(BubbleSort.sort(array)));
+    }
+    @Test
+    public void test01_checkInsertionSort() throws IOException {
+        Assert.assertTrue(isSorted(InsertionSort.sort(array)));
+    }
+    @Test
+    public void test01_checkShellSort() throws IOException {
+        Assert.assertTrue(isSorted(ShellSort.sort(array)));
+    }
+    @Test
+    public void test01_checkKStatisticMSort() throws IOException {
+        Assert.assertTrue(isSorted(KStatisticMSort.sort(array)));
+    }
+    @Test
+    public void test01_checkMergeSort() throws IOException {
+        Assert.assertTrue(isSorted(MergeSort.sort(array)));
+    }
+    @Test
+    public void test01_checkQuickSort() throws IOException {
+        Assert.assertTrue(isSorted(QuickSort.sort(array)));
+    }
+    @Test
+    public void test01_checkInsertionBinSort() throws IOException {
+        Assert.assertTrue(isSorted(InsertionBinSort.sort(array)));
     }
 
 }
